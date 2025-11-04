98324da738670ed18dd663fb4cc2022dabcc8b73
Javier-Torrecilla-Jimenez <jtorrecjim@educacion.navarra.es>
2025-11-04 13:01:46 +0100

    Update PRUEBA ANFITRION.ps1

--- PRUEBA ANFITRION.ps1
+++ PRUEBA ANFITRION.ps1
@@ -2,9 +2,9 @@
 #Ruta de origen de los archivos
 $origen = "C:\Users\torre\Downloads"
 
-#Ruta destino archivos con extensión .pdf
-$destino_pdf = "D:\pruebas\.pdf"
+#Ruta destino archivos con extension .pdf
+$destino_pdf = "C:\Users\torre\Downloads\Pruebas\PDF"
 
-Move-Item -Path "C:\Users\torre\Downloads\*.pdf" -Destination "D:\pruebas\.pdf" -Force
+Move-Item -path C:\Users\torre\Downloads\*.pdf -Destination $destino_pdf -Force
 
 
