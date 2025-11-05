3cd50a44b33b5914f37ce38128a749105944d7fa
Javier-Torrecilla-Jimenez <jtorrecjim@educacion.navarra.es>
2025-11-05 13:20:06 +0100

    Update Prueba Anfitrion.py

--- Prueba Anfitrion.py
+++ Prueba Anfitrion.py
@@ -2,20 +2,20 @@ import os
 import shutil
 
 ruta_origen = "C:\\users\\torre\\Downloads"
+ruta_destino = "D:\\Pruebas"
 
 carpetas_destino = {
-    ".txt": "D:\pruebas\.txt", #Indicamos la extensión y su carpeta. 
-    ".pdf": "D:\pruebas\.pdf",
-    ".jpg": "D:\pruebas\.jpg",
-    ".ova": "D:\pruebas\.ova",
-    ".pptx": "D:\pruebas\.pptx",
-    ".docx": "D:\pruebas\.docx",
+    ".txt": ".txt", #Indicamos la extensión y su carpeta. 
+    ".pdf": ".pdf",
+    ".jpg": ".jpg",
+    ".ova": ".ova",
 }
-for extension in carpetas_destino:
-    if not os.path.exists(os.path.join(ruta_origen, extension)):
-        os.makedirs(os.path.join(ruta_origen, extension))
 
-for nombre_archivo in os.listdir(ruta_origen):
+for extension in carpetas_destino: #para las extensiones en carpetas destino
+    if not os.path.exists(os.path.join(ruta_origen, extension)): 
+        os.makedirs(os.path.join(ruta_destino, extension))
+
+for nombre_archivo in os.listdir(ruta_destino):
     # Obtén la extensión del archivo
     nombre, extension = os.path.splitext(nombre_archivo) 
     
@@ -23,12 +23,12 @@ for nombre_archivo in os.listdir(ruta_origen):
     if extension in carpetas_destino:
         # Construye las rutas de origen y destino
         ruta_origen_archivo = os.path.join(ruta_origen, nombre_archivo)
-        ruta_destino_archivo = os.path.join(ruta_origen, carpetas_destino[extension], nombre_archivo)
+        ruta_destino_archivo = os.path.join(ruta_destino, carpetas_destino[extension], nombre_archivo)
         
         # Mueve el archivo a la carpeta de destino
         shutil.move(ruta_origen_archivo, ruta_destino_archivo)
         print(f"'{nombre_archivo}' movido a la carpeta '{carpetas_destino[extension]}'")
-
+    
 
 
 
