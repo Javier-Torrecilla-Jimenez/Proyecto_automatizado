48db826b1785d55e83d2b042185525e50b3ba8d0
Javier-Torrecilla-Jimenez <jtorrecjim@educacion.navarra.es>
2025-11-06 11:44:17 +0100

    Update PRUEBA ANFITRION.ps1

--- PRUEBA ANFITRION.ps1
+++ PRUEBA ANFITRION.ps1
@@ -1,8 +1,11 @@
-
 #Ruta de origen de los archivos
 $origen = "C:\Users\torre\Downloads"
 
 #Ruta destino archivos con extension .pdf
-$destino_pdf = "D:\Pruebas2\PDF"
+$destinopdf = "D:\Pruebas2\PDF\"
+#con con el ! indicamos que solo se meta a la función del if si el test-path nos devuelve como respuesta false (no existe)
+if (!(Test-Path $destinopdf)) { 
+    new-item -ItemType Directory -Path $destinopdf -Force    #Fuerza a el sistema a crear la carpeta de la ruta $destino_pdf
+}
 
-copy-Item -path C:\Users\torre\Downloads\*.pdf -Destination $destino_pdf 
+copy-Item -path C:\Users\torre\Downloads\*.pdf -Destination $destinopdf 
