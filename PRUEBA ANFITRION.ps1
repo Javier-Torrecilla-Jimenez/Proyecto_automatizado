af167828ae0f6e9d4fe90f5d39f885a04ae1107d
Javier-Torrecilla-Jimenez <jtorrecjim@educacion.navarra.es>
2025-11-08 18:09:17 +0100

    Update PRUEBA ANFITRION.ps1

--- PRUEBA ANFITRION.ps1
+++ PRUEBA ANFITRION.ps1
@@ -4,5 +4,11 @@ $origen = "C:\Users\torre\Downloads"
 #Ruta destino archivos con extension .pdf
 $destinopdf = "D:\Pruebas2\PDF\"
 
+if (!(Test-Path $destinopdf)) {
+    New-Item -Path $destinopdf -ItemType "Directory" -force 
+    exit
+}
+
 copy-Item -path C:\Users\torre\Downloads\*.pdf -Destination $destinopdf 
+
 
