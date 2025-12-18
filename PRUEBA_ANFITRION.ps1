3875100f7934a7ad8903e028b61d2c0f8e299875
Javier-Torrecilla-Jimenez <jtorrecjim@educacion.navarra.es>
2025-12-18 12:10:27 +0100

    Update PRUEBA_ANFITRION.ps1

--- PRUEBA_ANFITRION.ps1
+++ PRUEBA_ANFITRION.ps1
@@ -3,15 +3,10 @@ $origen = "C:\Users\torre\Downloads\"
 $destino = "D:\Pruebas2\"
 
 $CarpDest = @{
-    txt = ".txt" #Indicamos la extensión y su carpeta. 
-    ova = ".ova"
-    jpg = ".jpg"
-    pdf = ".pdf"
 }
 
 $extd = Get-ChildItem -path $origen -File #Le da a la variable el valor de los items de la ruta de origen
 $exts = $extd.Extension.TrimStart('.') #Le da a la vairbale el valor de la extensiones pero le elimina el . del principio
-
 
 
 foreach ($ext2 in $extd.Extension){ #Bucle para cada objeto dentro de la variable ext
@@ -21,22 +16,20 @@ foreach ($ext2 in $extd.Extension){ #Bucle para cada objeto dentro de la variabl
         }
     }
 
-    foreach ($extension in $CarpDest.GetEnumerator()) {
+foreach ($extension in $CarpDest.GetEnumerator()) {
         $CarpeN = Join-path -Path $destino -ChildPath $extension.Value # crea la ruta de destino de cada valor de el diccionario y la agrupa en la variable CarpeN
-    if (!(Test-Path $CarpeN)) { #Analiza si la ruta de destino existe
-        New-Item -Path $CarpeN -ItemType "Directory" -force # si no existe lo crea
-     }
-
-    }
-    $extensionestipo = $extension.Value #damos el valor de las claves a la variable de extensionestipo
     
-    foreach ($extensionestipo2 in $Extensionestipo) {
-        write-host Extensiones tipo 1: $extensionestipo 
-        write-host Extensiones tipo 2: $extensionestipo2
-        Get-ChildItem -Path $origen -filter *$extensionestipo2 | Copy-Item -Destination $CarpeN #Coge todos los elementos de la ruta de origen con los valores de la variable y los copia en la ruta de destino
+        if (!(Test-Path $CarpeN)) { #Analiza si la ruta de destino existe
+            New-Item -Path $CarpeN -ItemType "Directory" -force # si no existe lo crea
     }
 
+    }
+    $ext2|ForEach-Object{
+        Get-ChildItem -Path $origen -filter *$ext2 | Move-Item -Destination $CarpeN #Coge todos los elementos de la ruta de origen con los valores de la variable y los copia en la ruta de destino
+        write-host $ext2
 }
+}
+
 
 
 
