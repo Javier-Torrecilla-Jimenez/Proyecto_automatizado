135a0b995a2228a137629ab3fe07816b8e96092a
Javier-Torrecilla-Jimenez <jtorrecjim@educacion.navarra.es>
2026-01-16 13:59:35 +0100

    Update Prueba1.py

--- Prueba1.py
+++ Prueba1.py
@@ -2,10 +2,9 @@ import os
 
 ruta_origen = "C:\\users\\torre\\Downloads"
 ruta_destino = "D:\\Pruebas3"
-    if not os.path.exists(os.path.join(ruta_origen, extension)): #Si no existe la carpeta de la extension en la ruta de origen
-            os.makedirs(os.path.join(ruta_destino, extension), exist_ok=True) #Crea las carpetas en la ruta de destino
-        #con el exist_ok indicamos que lo ignore si la carpeta ya existe anteriormente
-        with open("ValorVariable.txt", "w") as archivo:
-            archivo.write("0")
-            print("hola")
+
+if not os.path.exists(ruta_destino):
+    with open("ValorVariable.txt", "w") as archivo:
+        archivo.write("0")
+        print("hola")
  
