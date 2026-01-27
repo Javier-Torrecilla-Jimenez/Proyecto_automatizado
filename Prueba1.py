2c4adb076630b58cf64f2980ea418033d2424eaa
Javier-Torrecilla-Jimenez <jtorrecjim@educacion.navarra.es>
2026-01-27 10:37:56 +0100

    Update Prueba1.py

--- Prueba1.py
+++ Prueba1.py
@@ -3,8 +3,7 @@ import os
 ruta_origen = "C:\\users\\torre\\Downloads"
 ruta_destino = "D:\\Pruebas3"
 
-if not os.path.exists(ruta_destino):
-    with open("ValorVariable.txt", "w") as archivo:
-        archivo.write("0")
-        print("hola")
+ 
+
+ 
  
